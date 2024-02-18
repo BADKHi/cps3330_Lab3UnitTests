33f793b06faf5898b61490d9ddacd49b5b79beb3
Mekhi Wood-Brock <106398956+BADKHi@users.noreply.github.com>
2024-02-18 18:59:03 -0500

    Add files via upload

---
+++ MekhiRootLib/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace MekhiRootLib;
+
+public class Class1
+{
+    public static double MyMax(double number1, double number2)
+    {
+        double max;
+        if (number1 > number2)
+            max = number1;
+        else
+            max = number2;
+
+        Console.WriteLine($"The MAX value between {number1} and {number2} is {max}");
+        return max;
+    }
+}
